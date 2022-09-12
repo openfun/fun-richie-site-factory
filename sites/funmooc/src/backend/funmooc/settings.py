d28fdacddd1109f7dadc990cf5a04ff81cec85ca
Samuel Paccoud - France Université Numérique <samuel.paccoud@fun-mooc.fr>
2022-09-12 08:50:52 +0200

    🔧(demo) add a "help" page to the demo site
    This page is necessary because it is targetted by an internal link
    on the home page. If there is no page with a reverse ID "help", the
    home page raises a 500 error looking for it.

--- sites/funmooc/src/backend/funmooc/settings.py
+++ sites/funmooc/src/backend/funmooc/settings.py
@@ -823,6 +823,37 @@ class Development(Base):
     # Django Check SEO
     DJANGO_CHECK_SEO_FORCE_HTTP = True
 
+    # Note: This override can be simplified once a merge update is added in richie for PAGES_INFO
+    RICHIE_DEMO_PAGES_INFO = {
+        "annex": {
+            "title": {"en": "Annex", "fr": "Annexe"},
+            "in_navigation": False,
+            "template": "richie/single_column.html",
+            "children": {
+                "annex__about": {
+                    "title": {"en": "About", "fr": "A propos"},
+                    "in_navigation": True,
+                    "template": "richie/single_column.html",
+                },
+                "annex__sitemap": {
+                    "title": {"en": "Sitemap", "fr": "Plan de site"},
+                    "in_navigation": True,
+                    "template": "richie/single_column.html",
+                },
+                "help": {
+                    "title": {"en": "Help", "fr": "Aide"},
+                    "in_navigation": True,
+                    "template": "richie/single_column.html",
+                },
+                "login-error": {
+                    "title": {"en": "Login error", "fr": "Erreur de connexion"},
+                    "in_navigation": False,
+                    "template": "richie/single_column.html",
+                },
+            },
+        },
+    }
+
 
 class Test(Base):
     """Test environment settings"""
