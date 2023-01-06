07b9ef706fb90d5d274f31b2467b857eb3c0edd5
David THENON <sveetch@gmail.com>
2023-01-06 01:18:04 +0000

    💄(ademe) modified Course detail, Course glimpse templates and React component
    We modified Course glimpse template and React component to display related
    organizations count instead of main course organization when there is more than one
    related organization.
    
    Also update Sass component 'subheader' override to remove social badge top margin.
    
    Co-authored-by: jbpenrath <jb.penrath@gmail.com>

---
+++ sites/ademe/src/frontend/js/components/CourseGlimpse/index.tsx
@@ -0,0 +1,141 @@
+import React, { memo } from 'react';
+import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
+
+import { CommonDataProps } from 'richie-education/js/types/commonDataProps';
+import { Course } from 'richie-education/js/types/Course';
+import { Icon } from 'richie-education/js/components/Icon';
+import { CourseGlimpseFooter } from 'richie-education/js/components/CourseGlimpse/CourseGlimpseFooter';
+
+export interface CourseGlimpseProps {
+  course: Course;
+}
+
+const messages = defineMessages({
+  cover: {
+    defaultMessage: 'Cover',
+    description: 'Placeholder text when the course we are glimpsing at is missing a cover image',
+    id: 'components.CourseGlimpse.cover',
+  },
+  organizationIconAlt: {
+    defaultMessage: 'Organization',
+    description: 'Organization logo alternative text for screen reader users',
+    id: 'components.CourseGlimpse.organizationIconAlt',
+  },
+  organizationCountLabel: {
+    defaultMessage: 'Produced by {organizationCount, number} partners',
+    description: 'Organization title for multiple organizations',
+    id: 'components.CourseGlimpse.organizationCountLabel',
+  },
+  codeIconAlt: {
+    defaultMessage: 'Course code',
+    description: 'Course code logo alternative text for screen reader users',
+    id: 'components.CourseGlimpse.codeIconAlt',
+  },
+  categoryLabel: {
+    defaultMessage: 'Category',
+    description: 'Category label text for screen reader users',
+    id: 'components.CourseGlimpse.categoryLabel',
+  },
+});
+
+const CourseGlimpseBase = ({ context, course }: CourseGlimpseProps & CommonDataProps) => {
+  const intl = useIntl();
+  return (
+    <div className="course-glimpse">
+      {/* the media link is only here for mouse users, so hide it for keyboard/screen reader users.
+      Keyboard/sr will focus the link on the title */}
+      <div aria-hidden="true" className="course-glimpse__media">
+        <a tabIndex={-1} href={course.absolute_url}>
+          {/* alt forced to empty string because it's a decorative image */}
+          {course.cover_image ? (
+            <img
+              alt=""
+              sizes={course.cover_image.sizes}
+              src={course.cover_image.src}
+              srcSet={course.cover_image.srcset}
+            />
+          ) : (
+            <div className="course-glimpse__media__empty">
+              <FormattedMessage {...messages.cover} />
+            </div>
+          )}
+        </a>
+      </div>
+      <div className="course-glimpse__content">
+        <div className="course-glimpse__wrapper">
+          <h3 className="course-glimpse__title">
+            <a className="course-glimpse__link" href={course.absolute_url}>
+              <span className="course-glimpse__title-text">{course.title}</span>
+            </a>
+          </h3>
+          {course.organization_highlighted_cover_image && course.organizations.length < 2 ? (
+            <div className="course-glimpse__organization-logo">
+              {/* alt forced to empty string because the organization name is rendered after */}
+              <img
+                alt=""
+                sizes={course.organization_highlighted_cover_image.sizes}
+                src={course.organization_highlighted_cover_image.src}
+                srcSet={course.organization_highlighted_cover_image.srcset}
+              />
+            </div>
+          ) : null}
+          <div className="course-glimpse__metadata course-glimpse__metadata--organization">
+            <Icon
+              name="icon-org"
+              title={intl.formatMessage(messages.organizationIconAlt)}
+              size="small"
+            />
+            {course.organizations.length === 1 ? (
+              <span className="title">{course.organization_highlighted}</span>
+            ) : null}
+            {course.organizations.length > 1 ? (
+              <span className="title">
+                <FormattedMessage
+                  {...messages.organizationCountLabel}
+                  values={{
+                    organizationCount: course.organizations.length,
+                  }}
+                />
+              </span>
+            ) : null}
+          </div>
+          <div className="course-glimpse__metadata course-glimpse__metadata--code">
+            <Icon
+              name="icon-barcode"
+              title={intl.formatMessage(messages.codeIconAlt)}
+              size="small"
+            />
+            <span>{course.code || '-'}</span>
+          </div>
+        </div>
+        {course.icon ? (
+          <div className="course-glimpse__icon">
+            <span className="category-badge">
+              {/* alt forced to empty string because it's a decorative image */}
+              <img
+                alt=""
+                className="category-badge__icon"
+                sizes={course.icon.sizes}
+                src={course.icon.src}
+                srcSet={course.icon.srcset}
+              />
+              <span className="offscreen">
+                <FormattedMessage {...messages.categoryLabel} />
+              </span>
+              <span className="category-badge__title">{course.icon.title}</span>
+            </span>
+          </div>
+        ) : null}
+        <CourseGlimpseFooter context={context} course={course} />
+      </div>
+    </div>
+  );
+};
+
+const areEqual: (
+  prevProps: Readonly<CourseGlimpseProps & CommonDataProps>,
+  newProps: Readonly<CourseGlimpseProps & CommonDataProps>,
+) => boolean = (prevProps, newProps) =>
+  prevProps.context === newProps.context && prevProps.course.id === newProps.course.id;
+
+export const CourseGlimpse = memo(CourseGlimpseBase, areEqual);
