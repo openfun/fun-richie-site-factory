cd008cedfdbefdac5e988aea79012c2ecd102528
jbpenrath <jb.penrath@gmail.com>
2025-01-27 17:40:29 +0100

    🐛(funmooc) ignore admin paths from the MalformedQueryStringMiddleware
    With the new MalformedQueryStringMiddleware, currently admin endpoints
    are checked and it prevents editor to add plugin on a page... so we have
     to exclude those paths to the check. Furthermore, instead of raising a
     BadRequest exception we now return a HttpBadRequestResponse. In this
     way, the request workflow is stopped here and a 400 is returned to the
     client instead of a 500.

--- sites/funmooc/src/backend/base/tests/test_middleware.py
+++ sites/funmooc/src/backend/base/tests/test_middleware.py
@@ -1,6 +1,5 @@
 """Test suite for the base app middlewares."""
 
-from django.core.exceptions import BadRequest
 from django.http import HttpRequest
 from django.test import TestCase
 
@@ -19,14 +18,16 @@ class TestMalformedQueryStringMiddleware(TestCase):
         request.META["QUERY_STRING"] = "foo=bar?bar=foo"
         request.path = "/"
 
-        with self.assertRaises(BadRequest):
-            MalformedQueryStringMiddleware(request).process_request(request)
+        response = MalformedQueryStringMiddleware(request).process_request(request)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content, b"URL query string is malformed.")
 
         # Encoded chars should be decoded
         request.META["QUERY_STRING"] = "foo=bar%3Fbar=foo"
 
-        with self.assertRaises(BadRequest):
-            MalformedQueryStringMiddleware(request).process_request(request)
+        response = MalformedQueryStringMiddleware(request).process_request(request)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.content, b"URL query string is malformed.")
 
     def test_malformed_query_string_middleware_ignore_api_endpoint(self):
         """
@@ -37,7 +38,20 @@ class TestMalformedQueryStringMiddleware(TestCase):
         request.META["QUERY_STRING"] = "q=Hello?"
         request.path = "/api/v1.0/search/"
 
-        MalformedQueryStringMiddleware(request).process_request(request)
+        response = MalformedQueryStringMiddleware(request).process_request(request)
+        self.assertIsNone(response)
+
+    def test_malformed_query_string_middleware_ignore_admin_endpoint(self):
+        """
+        If the request path is an admin endpoint, the middleware should ignore it.
+        """
+        request = HttpRequest()
+        request.method = "GET"
+        request.META["QUERY_STRING"] = "?placeholder_id=1&cms_path=/en/news/test/?edit"
+        request.path = "/en/admin/cms/page/add-plugin/"
+
+        response = MalformedQueryStringMiddleware(request).process_request(request)
+        self.assertIsNone(response)
 
     def test_malformed_query_string_middleware_ignore_non_get_request(self):
         """
@@ -49,4 +63,5 @@ class TestMalformedQueryStringMiddleware(TestCase):
         request.META["QUERY_STRING"] = "q=Hello?"
         request.path = "/api/v1.0/search/"
 
-        MalformedQueryStringMiddleware(request).process_request(request)
+        response = MalformedQueryStringMiddleware(request).process_request(request)
+        self.assertIsNone(response)
