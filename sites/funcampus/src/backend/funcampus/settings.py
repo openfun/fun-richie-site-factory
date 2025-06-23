15be042370e4e77ebc12356fa0aeca1e5ba9c511
Wilfried BARADAT <wilfried.baradat@fun-mooc.fr>
2025-06-23 18:09:41 +0200

    🧱(funcampus) migrate media storage to Scaleway
    Media files are now stored on Scaleway instead of AWS.
    Static files are no longer proxied through a CDN, as Scaleway Edge Services do
    not support this feature.

--- sites/funcampus/src/backend/funcampus/settings.py
+++ sites/funcampus/src/backend/funcampus/settings.py
@@ -18,6 +18,8 @@ from base.utils import merge_dict
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join("/", "data")
 
+# pylint: disable=too-many-lines
+
 
 def get_release():
     """Get the current release of the application.
@@ -935,36 +937,54 @@ class Production(Base):
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
 
-    STORAGES = {
-        "default": {
-            "BACKEND": "richie.apps.core.storage.MediaStorage",
+    # Using the S3Storage to be able to change the custom domain for media files without
+    # interfering with static files. Using the CDNManifestStaticFilesStorage with an
+    # undefined CDN_DOMAIN to use the custom `post_process` method.
+    STORAGES = values.DictValue(
+        {
+            "default": {
+                "BACKEND": values.Value(
+                    "storages.backends.s3.S3Storage",
+                    environ_name="STORAGES_DEFAULT_BACKEND",
+                ),
+                "OPTIONS": values.DictValue(
+                    {},
+                    environ_name="STORAGES_DEFAULT_OPTIONS",
+                ),
+            },
+            "staticfiles": {
+                "BACKEND": values.Value(
+                    "richie.apps.core.storage.CDNManifestStaticFilesStorage",
+                    environ_name="STORAGES_STATICFILES_BACKEND",
+                ),
+                "OPTIONS": values.DictValue(
+                    {},
+                    environ_name="STORAGES_STATICFILES_OPTIONS",
+                ),
+            },
         },
-        "staticfiles": {
-            "BACKEND": "richie.apps.core.storage.CDNManifestStaticFilesStorage",
-        },
-    }
+        environ_name="STORAGES",
+    )
     AWS_DEFAULT_ACL = None
     AWS_LOCATION = "media"
 
     AWS_ACCESS_KEY_ID = values.SecretValue()
     AWS_SECRET_ACCESS_KEY = values.SecretValue()
+    AWS_S3_REGION_NAME = values.Value("fr-par")
+    AWS_S3_ENDPOINT_URL = values.Value("https://s3.fr-par.scw.cloud")
 
     AWS_S3_OBJECT_PARAMETERS = {
         "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
         "CacheControl": "max-age=94608000",
     }
 
-    AWS_S3_REGION_NAME = values.Value("eu-west-1")
+    # CDN domain used to serve media files through the S3Storage
+    AWS_S3_CUSTOM_DOMAIN = values.Value(None)
+    AWS_STORAGE_BUCKET_NAME = values.Value("production-funcampus-media")
+    AWS_S3_FILE_OVERWRITE = values.Value(False)
 
-    AWS_MEDIA_BUCKET_NAME = values.Value("production-funcampus-media")
-
-    # CDN domain for static/media urls. It is passed to the frontend to load built chunks
+    # CDN domain for static urls. It is passed to the frontend to load built chunks.
     CDN_DOMAIN = values.Value()
-
-    @property
-    def TEXT_CKEDITOR_BASE_PATH(self):  # pylint: disable=invalid-name
-        """Configure CKEditor with an absolute url as base path to point to CloudFront."""
-        return f"//{self.CDN_DOMAIN}/static/djangocms_text_ckeditor/ckeditor/"
 
 
 class Feature(Production):
@@ -974,7 +994,7 @@ class Feature(Production):
     nota bene: it should inherit from the Production environment.
     """
 
-    AWS_MEDIA_BUCKET_NAME = values.Value("feature-funcampus-media")
+    AWS_STORAGE_BUCKET_NAME = values.Value("feature-funcampus-media")
 
 
 class Staging(Production):
@@ -984,7 +1004,7 @@ class Staging(Production):
     nota bene: it should inherit from the Production environment.
     """
 
-    AWS_MEDIA_BUCKET_NAME = values.Value("staging-funcampus-media")
+    AWS_STORAGE_BUCKET_NAME = values.Value("staging-funcampus-media")
 
 
 class PreProduction(Production):
@@ -994,4 +1014,4 @@ class PreProduction(Production):
     nota bene: it should inherit from the Production environment.
     """
 
-    AWS_MEDIA_BUCKET_NAME = values.Value("preprod-funcampus-media")
+    AWS_STORAGE_BUCKET_NAME = values.Value("preprod-funcampus-media")
