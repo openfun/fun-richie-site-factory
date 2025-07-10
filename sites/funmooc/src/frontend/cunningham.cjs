269e12d18b689f89c4d7485a491de46f7aa95eaa
Nicolas Clerc <kernicpanel@nclerc.fr>
2025-07-10 11:11:46 +0200

    💄(funmooc) change course glimpse footer color for paid offer
    Paid offer color should be replaced with #088488

--- sites/funmooc/src/frontend/cunningham.cjs
+++ sites/funmooc/src/frontend/cunningham.cjs
@@ -59,7 +59,7 @@ module.exports = {
           'robin-egg-blue': '#4fd0e7',
           'pastel-blue': '#7ea8ce',
           'ocean-blue': '#0069b3',
-          'eastern-blue': '#1BA0A8',
+          'eastern-blue': '#088488',
           'darkish-blue': '#002d7f',
           'navy-blue': '#001f50',
           'lipstick': '#e51a2d',
